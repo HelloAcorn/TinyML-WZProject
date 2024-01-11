2d126a0a2fe38b8d781dc9cd926a572581f1f251
민윤홍 <77008882+WzAcorn@users.noreply.github.com>
2024-01-11 15:34:12 +0900

    feat: New file
    데이터 수집, 모델평가 코드 추가.

--- getdata.py
+++ getdata.py
@@ -1,24 +1,28 @@
 import serial
 import csv
+import os
 from datetime import datetime
 
 # 시리얼 포트 설정
-ser = serial.Serial('COM13', 9600)  # COM포트 부분은 아두이노가 연결된 포트로 변경해야 합니다.
+ser = serial.Serial('COM13', 9600)
 
 # CSV 파일 설정
 filename = "sensor_data.csv"
-with open(filename, 'a', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Timestamp", "Low1_Humidity", "Low1_Temperature", "Low2_Humidity", "Low2_Temperature", 
-                     "Low3_Humidity", "Low3_Temperature", "Low4_Humidity", "Low4_Temperature", 
-                     "High_Humidity", "High_Temperature"])  # 컬럼명 설정
+file_exists = os.path.isfile(filename)  # 파일이 이미 존재하는지 확인
 
-    print("온,습도를 기록중입니다...")
+with open(filename, 'a', newline='') as file:  # 'a' 모드로 파일 열기
+    writer = csv.writer(file)
+    if not file_exists:
+        writer.writerow(["Timestamp", "Low1_Humidity", "Low1_Temperature", "Low2_Humidity", "Low2_Temperature", 
+                         "Low3_Humidity", "Low3_Temperature", "Low4_Humidity", "Low4_Temperature", 
+                         "High_Humidity", "High_Temperature"])  # 파일이 새로 생성되면 컬럼명 추가
+
+    print("온,습도가 기록중입니다...")
     while True:
         if ser.in_waiting:
-            data = ser.readline().decode('utf-8').rstrip()  # 데이터 읽기
-            if data:  # 데이터가 존재하면
-                data_list = data.split(" ")  # 공백으로 데이터 분리
-                data_list.insert(0, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))  # 현재 시간 추가
-                writer.writerow(data_list)  # CSV 파일에 작성
+            data = ser.readline().decode('utf-8').rstrip()
+            if data and "Failed to read from DHT" not in data:
+                data_list = data.split(" ")
+                data_list.insert(0, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                writer.writerow(data_list)
                 file.flush()
